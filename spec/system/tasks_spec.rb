55cf766e923da864a8dee53b6c3ab93d7551da6f
DaichiNiwa <daichi.niwa.100@gmail.com>
2021-12-31 13:42:30 +0900

    fix: shard_examplesを使った

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'タスク管理機能', type: :system do
   let(:user_a) { FactoryBot.create(:user, name: 'ユーザーA', email: 'a@example.com') }
   let(:user_b) { FactoryBot.create(:user, name: 'ユーザーB', email: 'b@example.com') }
-  let!(:task_a) {FactoryBot.create(:task, name: '最初のタスク', user: user_a)}
+  let!(:task_a) { FactoryBot.create(:task, name: '最初のタスク', user: user_a) }
 
   before do
     visit login_path
@@ -12,13 +12,15 @@ describe 'タスク管理機能', type: :system do
     click_button 'ログインする'
   end
 
+  shared_examples_for 'ユーザーAが作成したタスクが表示される' do
+    it { expect(page).to have_content '最初のタスク' }
+  end
+
   describe '一覧表示機能' do
     context 'ユーザーAがログインしているとき' do
       let(:login_user) { user_a }
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
 
     context 'ユーザーBがログインしているとき' do
@@ -32,15 +34,13 @@ describe 'タスク管理機能', type: :system do
 
   describe '詳細表示機能' do
     context 'ユーザーAがログインしているとき' do
-      let(:login_user) { user_a}
+      let(:login_user) { user_a }
 
       before do
         visit task_path(task_a)
       end
 
-      it 'ユーザーAが作成したタスクが表示される' do
-        expect(page).to have_content '最初のタスク'
-      end
+      it_behaves_like 'ユーザーAが作成したタスクが表示される'
     end
   end
 end
