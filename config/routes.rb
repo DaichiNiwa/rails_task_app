cf599b6d448cdd38e466b8fe15b0eee525ec3645
DaichiNiwa <daichi.niwa.100@gmail.com>
2022-01-01 21:07:33 +0900

    Revert "add: タスク登録時に確認画面を挟むようにした"
    This reverts commit cd1a861866f920e6eae1f405a9550d760257be79.

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,6 @@ Rails.application.routes.draw do
   end
 
   root to: 'tasks#index'
-  resources :tasks do
-    post :confirm, action: :confirm_new, on: :new
-  end
+  resources :tasks
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
